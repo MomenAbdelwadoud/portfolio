7bd0f30e1dd3e19a8a4acf3dc51d6792b6ffb7c9
MomenAbdelwadoud <momenwadoudg@gmail.com>
2023-03-29 13:29:12 +0200

    Add CV

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Image from 'next/image'
-import ArrowDown from './svg/arrow-down'
+import Open from './svg/new-window'
 
 const Button = () => {
   return (
-    <a href="#contact">
+    <a  rel="noreferrer" target={"_blank"} href="https://drive.google.com/file/d/1rrCDZibL8jwFQZlQPN4BrPWjzN6hGRDg/view?usp=sharing">
       <button className='py-2 px-5 justify-center items-center bg-primary opacity-80 text-black rounded-lg flex gap-2 font-semibold text-sm'>
-          <p>Get in touch</p>
-              {/* <ArrowDown viewBox="-5 -5 25 25"></ArrowDown> */}
+          <p>Open CV</p>
+              <Open></Open>
       </button>
     </a>
   )
