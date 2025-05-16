7aad604ad43fa4cbb22408cf5b082570ecdfe1ae
MomenAbdelwadoud <momenwadoudg@gmail.com>
2025-05-16 12:33:04 +0300

    add metadata for SEO

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,29 @@ import Nav from "./components/nav";
 import Projects from "./components/projects";
 import ScrollDots from "./components/scroll-dots";
 
+export const metadata = {
+	title: "Momen Elkhalifa | Fullstack Developer",
+	description:
+		"A fullstack developer skilled in React, Next.js, TypeScript, and modern web technologies. View projects, experience, and contact information.",
+	keywords: [
+		"Momen Abdelwadoud",
+		"Momen Elkhalifa",
+		"Momen Abdelwadoud Elkhalifa",
+		"Fullstack Developer",
+		"Momen Portfolio",
+		"Momen Elkhalifa Portfolio",
+	],
+	authors: [{name: "Momen Abdelwadoud Elkhalifa"}],
+	openGraph: {
+		title: "Momen Elkhalifa | Fullstack Developer",
+		description:
+			"A fullstack developer skilled in React, Next.js, TypeScript, and modern web technologies.",
+		url: "https://momen.netlify.app",
+		siteName: "Momen Elkhalifa Portfolio",
+		type: "website",
+	},
+};
+
 const Home = () => {
 	return (
 		<div className="text-white">
